984b22d734b11016143b6d9a62941374c7d79976
yoshiaki44 <yo44ch@gmail.com>
2022-02-11 09:32:29 +0900

    最適化

--- src/layout/BlogLayout.tsx
+++ src/layout/BlogLayout.tsx
@@ -4,28 +4,12 @@ import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { LayoutErrorBoundary } from "./LayoutErrorBoundary";
 
-/**
- * @package
- */
-
-const SideMenu = () => {
-  return (
-    <div className="space-y-5 bg-white">
-      <div className="items-center p-[100px] w-full text-center bg-gray-400">広告</div>
-      <div className="py-[300px] w-full text-center bg-white">Profile</div>
-    </div>
-  );
-};
-
 export const BlogLayout: CustomLayout = (page) => {
   return (
-    <div className="grid grid-rows-[auto,1fr,auto] gap-5 min-h-screen bg-gray-200">
+    <div className="grid grid-rows-[auto,1fr,auto] gap-5 min-h-screen font-mono bg-[#f5ddb9]">
       <Header />
-      <main className="gap-5 px-3 text-center md:grid md:grid-cols-3 md:px-32">
-        <div className="col-span-2">
-          <LayoutErrorBoundary>{page}</LayoutErrorBoundary>
-        </div>
-        <SideMenu />
+      <main>
+        <LayoutErrorBoundary>{page}</LayoutErrorBoundary>
       </main>
       <Footer />
     </div>
